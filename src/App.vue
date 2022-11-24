42bca789a2d0f48e4bd6e58c3b36b8deef7c5a57
Luqman Fauzi <you@example.com>
2022-11-24 18:03:31 +0800

    add new page for menu

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/new-page">New Page</RouterLink>
+        <RouterLink :to="{name: 'menu'}">Menu</RouterLink>
       </nav>
     </div>
   </header>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NewPage from '../views/NewPage.vue'
+import Menu from "../views/Menu.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,7 +23,12 @@ const router = createRouter({
       path: '/new-page',
       name: 'newPage',
       component: NewPage
-    }
+    },
+    {
+      path: '/menu',
+      name: 'menu',
+      component: Menu
+    },
   ]
 })
 
